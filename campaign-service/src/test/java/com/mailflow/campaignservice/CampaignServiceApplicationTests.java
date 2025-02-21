121b35b050345da4110278777d62a2e23c35130f
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-02-21 16:53:53 +0100

    setup campaign service

---
+++ campaign-service/src/test/java/com/mailflow/campaignservice/CampaignServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mailflow.campaignservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
