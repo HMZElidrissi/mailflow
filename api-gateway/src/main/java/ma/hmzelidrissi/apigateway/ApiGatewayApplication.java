0870728de17bac78850e0cc3f0f1dea512ff3e53
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-03-10 22:15:59 +0000

    add api gateway

---
+++ api-gateway/src/main/java/ma/hmzelidrissi/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package ma.hmzelidrissi.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiGatewayApplication.class, args);
+  }
+}
